5f9b019b5fa095386b7eb1e97c202a728c1af326
GuillemGauchia <guillem.gauchia01@estudiant.upf.edu>
2024-04-29 18:43:42 +0200

    FullProject

---
+++ Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+    public float horizontalBoundary = 22;
+    public float movementSpeed = 14f;
+    public GameObject hayBalePrefab; // Reference to the Hay Bale prefab.
+    public Transform haySpawnpoint; // The point from which the hay will to be shot.
+    public float shootInterval = 1.0f; // The smallest amount of time between shots
+    private float shootTimer; // A timer that to keep track whether the machine can shoot or not
+    public Transform modelParent; // Parent transform for the model
+
+    // Model prefabs
+    public GameObject blueModelPrefab;
+    public GameObject yellowModelPrefab;
+    public GameObject redModelPrefab;
+
+    void Start()
+    {
+        LoadModel();
+        StartCoroutine(IncreaseMovementSpeed());
+        StartCoroutine(DecreaseShootInterval());
+    }
+
+    private void LoadModel()
+    {
+        Destroy(modelParent.GetChild(0).gameObject); // Destroy previous model
+        switch (GameSettings.hayMachineColor) // Load selected model
+        {
+            case HayMachineColor.Blue:
+                Instantiate(blueModelPrefab, modelParent);
+                break;
+            case HayMachineColor.Yellow:
+                Instantiate(yellowModelPrefab, modelParent);
+                break;
+            case HayMachineColor.Red:
+                Instantiate(redModelPrefab, modelParent);
+                break;
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary)
+        {
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary)
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+        SoundManager.Instance.PlayShootClip();
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    IEnumerator IncreaseMovementSpeed()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5.0f);
+            if (movementSpeed < 30.0f) // Increase speed only if it's less than 25 units
+            {
+                movementSpeed += 1.0f;
+            }
+        }
+    }
+
+    IEnumerator DecreaseShootInterval()
+    {
+        int decreaseCount = 0;
+        while (decreaseCount < 4) 
+        {
+            yield return new WaitForSeconds(10.0f); 
+            shootInterval -= 0.1f; 
+            shootInterval = Mathf.Max(shootInterval, 0.1f); 
+            decreaseCount++; 
+        }
+    }
+
+    void Update()
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+}
---
+++ Assets/RW/Scripts/SheepSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepSpawner : MonoBehaviour
+{
+    public bool canSpawn = true;
+    public GameObject sheepPrefab;
+    public List<Transform> sheepSpawnPositions = new List<Transform>();
+    private float timeBetweenSpawns = 2.0f;
+    private List<GameObject> sheepList = new List<GameObject>();
+    private int spawnReductionCount = 0;
+
+    void Start()
+    {
+        //timeBetweenSpawns = Random.Range(1.0f, 3.0f);
+        StartCoroutine(SpawnRoutine());
+        StartCoroutine(ReduceSpawnTime());
+    }
+
+    private void SpawnSheep()
+    {
+        Vector3 randomPosition = sheepSpawnPositions[Random.Range(0, sheepSpawnPositions.Count)].position;
+        GameObject sheep = Instantiate(sheepPrefab, randomPosition, sheepPrefab.transform.rotation);
+        sheepList.Add(sheep);
+        sheep.GetComponent<Sheep>().SetSpawner(this);
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (canSpawn)
+        {
+            SpawnSheep();
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
+    }
+
+    private IEnumerator ReduceSpawnTime()
+    {
+        while (spawnReductionCount < 15)
+        {
+            yield return new WaitForSeconds(5.0f);
+            timeBetweenSpawns -= 0.1f;
+            spawnReductionCount++;
+        }
+    }
+
+    public void RemoveSheepFromList(GameObject sheep)
+    {
+        sheepList.Remove(sheep);
+    }
+
+    public void DestroyAllSheep()
+    {
+        foreach (GameObject sheep in sheepList)
+        {
+            Destroy(sheep);
+        }
+
+        sheepList.Clear();
+    }
+
+    void Update()
+    {
+
+    }
+}
